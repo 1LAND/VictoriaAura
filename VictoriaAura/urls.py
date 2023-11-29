0350951d884a8c402c42f669761839b6987906ef
Dl0rd <124901949+Dl0rd@users.noreply.github.com>
2023-11-29 22:41:33 +0700

    v1.1.0

--- VictoriaAura/urls.py
+++ VictoriaAura/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 
 from backend.views import ImgViewSet,GameDisciplineViewSet,NewsViewSet,UserViewSet,TeamViewSet
+from backend.views import donation_alerts_login,donation_alerts
 
 from rest_framework.routers import SimpleRouter
 
@@ -19,5 +20,6 @@ router.register("team",TeamViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-
+    path('api/donation_alerts_login/', donation_alerts_login),
+    path('api/donation_alerts/', donation_alerts),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/views.py
+++ backend/views.py
@@ -1,11 +1,51 @@
+from django.http.response import HttpResponseRedirect
+
+from django.http.request import HttpRequest
+
+import requests
+
 from rest_framework.response import Response
-from rest_framework.decorators import action
+from rest_framework.decorators import action,api_view
 from rest_framework.viewsets import ModelViewSet
 
 
 from .serializers import NewsSerializers,ImgSerializer,GameDisciplineSerializer,UserMinInfoSerializer,UserMaxInfoSerializer,TeamSerializer
 from .models import News,ImgFiles,GameDiscipline,User,Team
 from rest_framework.permissions import IsAdminUser
+
+from config import CONFIG_DONATION_ALERT as CDA
+
+DEFAULT_URL = "https://www.donationalerts.com/oauth/"
+DEFAULT_API_LINK = "https://www.donationalerts.com/api/v1/"
+
+
+def get_access_token(code):
+    return requests.post(
+        f"https://www.donationalerts.com/oauth/token",
+        data={
+            "grant_type":"authorization_code",
+            "client_id":CDA['client_id'],
+            "client_secret":CDA['client_secret'],
+            "redirect_uri":CDA['redirect_uri'],
+            "code":code
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
+        ).json()['access_token']
+
+
+@api_view(['GET'])
+def donation_alerts_login(request):
+    if request.method == 'GET':
+        return HttpResponseRedirect(redirect_to=f"{DEFAULT_URL}authorize?client_id={CDA['client_id']}&redirect_uri={CDA['redirect_uri']}&response_type=code&"\
+        "scope=oauth-user-show%20oauth-donation-subscribe%20oauth-donation-index%20oauth-custom_alert-store%20oauth-goal-subscribe%20oauth-poll-subscribe")
+
+@api_view(['GET'])
+def donation_alerts(request):
+    code = request.GET.get("code")
+    access_token = get_access_token(code)
+    objs = requests.get(f"{DEFAULT_API_LINK}alerts/donations?page=1", headers={"Authorization": f"Bearer {access_token}"}).json()
+    return Response([{"group":i['username'],"users":i['message'],"amount":i['amount'],"currency": i["currency"]} for i in objs['data']])
+
 
 
 class TeamViewSet(ModelViewSet):
